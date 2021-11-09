885f475f7241684f438f4d2989b0fd7167ef8896
Yash Charpe <yashrcharpe07@gmail.com>
2021-11-09 17:02:51 +0530

    Added Login Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexPageComponent } from './MyComponents/index-page/index-page.component';
+import { RouterModule, Routes } from '@angular/router';
+
+import {LoginPageComponent} from './MyComponents/login-page/login-page.component'
+
+const appRoutes:Routes = [
+  {path:'',component:IndexPageComponent},
+  {path:'login',component:LoginPageComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexPageComponent
+    IndexPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
