d7bac3015cf440b685c713eac3dfc456d8b55f4c
Anurag Taparia <anuragtaparia14@gmail.com>
2021-11-20 10:48:59 +0530

    navbar and footer

--- src/app/MyComponents/main-dashboard-page/main-dashboard-page.component.ts
+++ src/app/MyComponents/main-dashboard-page/main-dashboard-page.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainDashboardPageComponent implements OnInit {
 
+
+  counter = 0;
+  audio = new Audio()
+
   constructor() { }
 
   ngOnInit(): void {
+    this.audio.src = "../../../assets/Music/New-Release-Songs/Ranjha - Shershaah 128 Kbps.mp3"
+    this.counter = 0;
   }
+  
+  playAudio(){
 
+    this.counter++
+    if(this.counter%2==0)
+    {
+      this.audio.pause()
+      console.log("even: "+this.counter)
+    }
+    else{
+      this.audio.play()
+      console.log("odd: "+this.counter)
+    }
+
+  }
 }
